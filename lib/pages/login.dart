4016b19c4ab9d6f4322bc67370fd3377e8a7bd5d
Anurag <kumarayush1014@gmail.com>
2024-01-12 16:57:47 +0530

    added google auth and made login screen

---
+++ lib/pages/login.dart
@@ -0,0 +1,280 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  bool inputTextNotNull = false;
+  bool emailValid = true;
+  String _email = '';
+  bool loading = false;
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  User? _user;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    auth.authStateChanges().listen((event) {
+      print(event);
+      setState(() {
+        _user = event;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: const Color(0xFFDCDADD),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: deviceWidth * 0.2),
+            SvgPicture.asset(
+              'assets/images/logo.svg',
+              height: deviceWidth * 0.35,
+            ),
+            SizedBox(height: deviceWidth * 0.1),
+            Text(
+              "Welcome Back, you've been missed",
+              style: TextStyle(
+                fontSize: deviceWidth * 0.04,
+                color: Colors.grey[800],
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(deviceWidth * 0.05),
+              child: TextField(
+                cursorColor: Colors.grey[900],
+                onChanged: (text) {
+                  setState(() {
+                    emailValid = true;
+                    _email = text.trim();
+                    if (emailController.text.length >= 5) {
+                      inputTextNotNull = true;
+                    } else {
+                      inputTextNotNull = false;
+                    }
+                  });
+                },
+                controller: emailController,
+                decoration: emailValid
+                    ? InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: const BorderSide(
+                                width: 0, style: BorderStyle.none)),
+                        labelText: 'Email',
+                        fillColor: Colors.white,
+                        filled: true)
+                    : const InputDecoration(
+                        errorText: 'Please Enter a valid Email'),
+              ),
+            ),
+            SizedBox(height: deviceWidth * 0.02),
+            inputTextNotNull
+                ? GestureDetector(
+                    onTap: () async {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                      bool res = isValidEmail(_email);
+                      setState(() {
+                        emailValid = res;
+                        loading = true;
+                      });
+                      if (!res) {
+                        setState(() {
+                          emailValid = false;
+                          loading = false;
+                        });
+                        return;
+                      }
+                    },
+                    child: Container(
+                      width: deviceWidth * .90,
+                      height: deviceWidth * .14,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(5)),
+                      ),
+                      child: loading
+                          ? Center(
+                              child: SizedBox(
+                                height: deviceWidth * 0.08,
+                                width: deviceWidth * 0.08,
+                                child: const CircularProgressIndicator(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          : Center(
+                              child: Text(
+                                'Send OTP',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: deviceWidth * .040,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                    ),
+                  )
+                : Container(
+                    width: deviceWidth * .90,
+                    height: deviceWidth * .14,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      borderRadius: const BorderRadius.all(Radius.circular(5)),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Send OTP',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: deviceWidth * .040,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+            SizedBox(
+              height: deviceWidth * .05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 1,
+                  width: deviceWidth * .25,
+                  color: const Color(0xffA2A2A2),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Or continue with',
+                  style: TextStyle(
+                    fontSize: deviceWidth * .040,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 1,
+                  width: deviceWidth * .25,
+                  color: const Color(0xffA2A2A2),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: deviceWidth * .1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    googleSignin();
+                  },
+                  child: Container(
+                      width: deviceWidth * 0.25,
+                      height: deviceWidth * 0.25,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(deviceWidth * 0.05)),
+                      ),
+                      child: Center(
+                          child: SvgPicture.asset(
+                        'assets/icons/google.svg',
+                        height: deviceWidth * 0.17,
+                      ))),
+                ),
+                Container(
+                    width: deviceWidth * 0.25,
+                    height: deviceWidth * 0.25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.all(Radius.circular(deviceWidth * 0.05)),
+                    ),
+                    child: Center(
+                        child: SvgPicture.asset(
+                      'assets/icons/facebook.svg',
+                      height: deviceWidth * 0.17,
+                    ))),
+              ],
+            ),
+            SizedBox(
+              height: deviceWidth * .1,
+            ),
+            _user != null ? Text(_user!.displayName!) : Container(),
+            _user != null ? Text(_user!.email!) : Container(),
+            Container(
+              width: deviceWidth,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account? ",
+                        style: TextStyle(
+                          fontSize: deviceWidth * .035,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontSize: deviceWidth * .035,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool isValidEmail(email) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(email);
+  }
+
+  void googleSignin(){
+    try {
+      GoogleAuthProvider provider = GoogleAuthProvider();
+      auth.signInWithProvider(provider);
+    } catch(err){
+      print(err);
+    }
+  }
+}
