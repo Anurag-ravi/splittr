1a6eaaef7d36cc2916c4c0cf6b54a2eb73865f0e
Anurag Ravi <anurag.daan@gmail.com>
2026-05-12 00:17:24 +0530

    Integrate Firebase Cloud Messaging for push notifications and update dependencies
    - Added POST_NOTIFICATIONS permission in AndroidManifest.xml
    - Implemented Firebase Messaging setup in main.dart
    - Updated FCM token management in login.dart and profileScreen.dart
    - Removed JWT token generation utility
    - Updated pubspec.yaml and pubspec.lock for firebase_messaging dependency

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import firebase_messaging
 import flutter_contacts
 import share_plus
 import shared_preferences_foundation
@@ -15,6 +16,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterContactsPlugin.register(with: registry.registrar(forPlugin: "FlutterContactsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
